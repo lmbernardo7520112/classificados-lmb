0a8f723e1c9b8dfcafedc606043e579609c6894d
lmbernardo7520112 <lmbernardo752011@gmail.com>
2021-03-07 20:43:18 -0300

    Criando um sistema de classificados com Ruby on Rails - Aulas: 1-17

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,47 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ test "nome é obrigatório" do
+   
+  user = User.new(name: nil)
+
+  user.save 
+
+  assert user.errors[:name].any?
+ end
+
+
+ test "email é obrigatório e no formato válido" do 
+  user = User.new(email: nil)
+
+  user.save
+
+  assert user_errors[:email].any?
+
+  user = User.new(email: "inválido")
+  user.save
+  assert user.errors[:email].any?
+ end
+
+test "email é obrigatório e formato válido" do
+  user = User.new(email: nil)
+  user.save
+  assert user.errors[:email].any?
+
+  user = User.new(email: "invalido")
+  user.save
+  assert user.errors[:email].any?
+
+  #user = User.new(email: "valido@exemplo.com")
+  #user.save
+  #refute user.errors[:email].any?
+end
+
+    test "email é unico" do 
+        user = User.create!(name: "eu", email: "eu@example.com", password: "secret", password_confirmation: "secret")
+
+         other_user = User.create(email: "eu@example.com")
+
+         assert other_user.errors[:email].any?
+    end 
 end
